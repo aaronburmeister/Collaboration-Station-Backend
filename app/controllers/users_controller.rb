fc89d77e48225c718399e8543d416ef4013f81db
aaronburmeister <f34r91@gmail.com>
2020-03-25 13:22:04 -0600

    Add users_controller base methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+    before_action: :authenticate, only: :delete
+
+    def index
+        @users = User.all
+
+        render json: @users
+    end
+
+    def show
+        @user = User.find(params[:id])
+
+        render json: @user
+    end
+
+    def create
+        @user = User.create(user_params)
+
+        if @user.valid?
+            @user.save
+            render json: @user
+        else
+            render json: status: :unauthorized
+        end
+    end
+
+    def delete
+        @user = User.find(params[:id])
+
+        @user.destroy
+
+        render json: message: "User successfully deleted!"
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit({
+            username: params[:username],
+            password: params[:password],
+            email: params[:email],
+            isArtist: params[:isArtist]
+        })
+    end
+
 end
